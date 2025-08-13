fdc3654854b8ab5e88879ff17a4f38f40250cc43
dipalak1234 <109249554+dipalak1234@users.noreply.github.com>
2025-08-13 08:34:45 +0530

    Add files via upload

---
+++ Module3_task_2_python_tutedude.py
@@ -0,0 +1,15 @@
+'''
+sum=0
+for i in range(1,51):
+    sum=sum+i
+print(sum)
+'''
+end=50
+count=1
+sum=0
+while (count<=end):
+    #sum=sum+count
+    sum=sum+count
+    count=count+1
+print("The sum of numbers from 1 to 50 is: ",sum)
+
---
+++ module3_task_1_python_tutedude.py
@@ -0,0 +1,6 @@
+a=input('Enter a number: ')
+a=int(a)
+if(a%2==0):
+    print(a,' is an even number.')
+else:
+    print(a,' is an odd number.')
